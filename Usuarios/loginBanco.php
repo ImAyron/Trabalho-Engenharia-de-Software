c73e41cdd829aa604f540a7656f6e9adba53be47
LucasBrazza <lucas.brazza.lb@gmail.com>
2021-12-22 18:45:27 -0300

    created user login

---
+++ Usuarios/loginBanco.php
@@ -0,0 +1,30 @@
+<?php
+
+require '../Database/conexao.php';
+
+$user = null;
+
+$email = $_POST["email"];
+$senha = $_POST['senha'];
+
+$stmt = $conection->prepare("SELECT * FROM usuarios WHERE email=:email AND senha=:senha");
+$stmt->bindParam(':email', $email);
+$stmt->bindParam(':senha', $senha);
+$stmt->execute();
+
+$stmt = $stmt->fetch();
+
+if (empty($stmt)) {
+  echo "<script>alert('Login ou senha incorretos!'); location.replace('../index.php');</script>";
+  die();
+} else {
+
+  session_start();
+  $_SESSION['email'] = $stmt['email'];
+  $_SESSION['nome'] = $stmt['nome'];
+  $_SESSION['matricula'] = $stmt['matricula'];
+
+
+
+  header('location:../Reservas/index.php');
+}
